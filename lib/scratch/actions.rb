4f763de50f9745eab898be65f5002f38fbba67ab
ik5 <idok@linesip.com>
2013-05-08 19:00:03 +0300

    Starting to work on init part

--- lib/scratch/actions.rb
+++ lib/scratch/actions.rb
@@ -5,6 +5,10 @@ module Scratch
     class Exec
 
       def self.init(raw_args, args)
+        if raw_args.length != 1
+          $stderr.puts "[#{CLI::to_color('error', :red)}] No name was provided for the project"
+          exit(-3)
+        end
       end
 
       def self.test(raw_args, args)
