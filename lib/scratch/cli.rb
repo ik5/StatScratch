4204ea216385346e8d1c0f53fff1dc06334cd25e
ik5 <idok@linesip.com>
2013-05-08 17:20:46 +0300

    First command to be implemented

--- lib/scratch/cli.rb
+++ lib/scratch/cli.rb
@@ -7,7 +7,13 @@ module Scratch
 
   class CliParser
     def self.parse(args)
-      
+      opts = Slop.parse(args) do 
+        command :init do
+          
+        end # command 'init' do
+      end # Slop.parse(args)
+
+      opts
     end # def self.parse
 
   end # class CliParser
