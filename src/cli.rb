1d25e0b84996fc5ca92b4b2590a9748541268122
ik5 <idok@linesip.com>
2013-05-08 11:09:48 +0300

    Trigger actions to actions

--- src/cli.rb
+++ src/cli.rb
@@ -2,6 +2,9 @@
 
 module Scratch
   DEFAILT_ACTION = :test
+
+  ACTIONS        = [:init, :test, :publish]
+
   class CliParser
     def self.parse(args)
       opt_parser = OptionParser.new do |opts|
@@ -34,5 +37,35 @@ module Scratch
     end # def self.parse
 
   end # class CliParser
+
+  class ToAction
+    def initialize
+       @action = CliParser.parse
+    end # def initialize
+
+    def run 
+       if @action.kind_of? Symbol
+         # White list of actions. 
+         # Do not allow non white list to execute - security risk
+         if ACTIONS.include? @action
+           Actions.send(@action)
+         else
+           $stderr.puts 'I\'llgal action'
+           exit(-1)
+         end # if ACTIONS.include? action
+       elsif @action.kind_of? String
+         puts @action
+         exit(0)
+       elsif @action.kind_of? NilClass
+         # No action
+         Actions.send(DEFAILT_ACTION)
+       else
+         $stderr.puts 'Unknown action was provided.'
+         exit(-1)
+       end # if action.kind_of? Symbol
+    end # def run 
+
+  end # class ToAction
+
 end # module Scratch
 
