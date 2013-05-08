e2cf71f23620daddcf0caf9711acfda27c50915c
ik5 <idok@linesip.com>
2013-05-08 17:21:11 +0300

    Working on the parser itself again

--- lib/scratch/cli.rb
+++ lib/scratch/cli.rb
@@ -24,25 +24,6 @@ module Scratch
     end # def initialize
 
     def run 
-       if @action.kind_of? Symbol
-         # White list of actions. 
-         # Do not allow non white list to execute - security risk
-         if ACTIONS.include? @action
-           Actions.send(@action)
-         else
-           $stderr.puts 'I\'llgal action'
-           exit(-1)
-         end # if ACTIONS.include? action
-       elsif @action.kind_of? String
-         puts @action
-         exit(0)
-       elsif @action.kind_of? NilClass
-         # No action
-         Actions.send(DEFAILT_ACTION)
-       else
-         $stderr.puts 'Unknown action was provided.'
-         exit(-1)
-       end # if action.kind_of? Symbol
     end # def run 
 
   end # class ToAction
