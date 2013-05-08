f130f5cb515647dfcc41b2478763ef5a842d6662
ik5 <idok@linesip.com>
2013-05-08 17:16:00 +0300

    Fixing CLI parser. no need for the basic ruby parser

--- bin/scratch.rb
+++ bin/scratch.rb
@@ -2,7 +2,6 @@
 # encoding: utf-8
 
 require 'rubygems'
-require 'optparse'
 
 begin
   require 'bundler'
--- lib/scratch/cli.rb
+++ lib/scratch/cli.rb
@@ -1,4 +1,4 @@
-
+# encoding: utf-8
 
 module Scratch
   DEFAILT_ACTION = :test
@@ -7,33 +7,7 @@ module Scratch
 
   class CliParser
     def self.parse(args)
-      opt_parser = OptionParser.new do |opts|
-        opts.banner = 'Usage: scratch.rb [action]'
-        opts.separtor ''
-        opts.separtor 'Action:'
-
-        opts.on('init', 'Create new project') do |act|
-          return :init
-        end
-
-        opts.on('test', 'Test files on draft') do |act|
-          return :test
-        end
-
-        opts.on('publish', 'Publish files on drafts that are set to false') do |act|
-          return :publish
-        end
-
-        opts.on_tail('-h', 'help', 'Display help screen') do
-          return opts
-        end
-
-        opts.on_tail('--version', 'Show version') do
-          return VERSION
-        end
-      end # opt_parser
-
-      opt_parser.parse!(args)
+      
     end # def self.parse
 
   end # class CliParser
