087be98e641304b5a7db44c52da0bd2618ec9b2c
ik5 <idok@linesip.com>
2013-05-09 17:41:20 +0300

    Some Error codes

---
+++ lib/scratch/return_codes.rb
@@ -0,0 +1,10 @@
+
+module Scratch 
+  GEM_NOT_FOUND       = -1
+  INVALID_CLI_ACTION  = -2
+  NO_PROJECT_NAME     = -3
+  NO_PREMISSION_ERROR = -4
+  UNKNOWN_ERROR       = -5
+  FILE_NOT_FOUND      = -6
+  SELF_COPY_FILE      = -7
+end
