0316407d287ce36841f44ece4a7f2c5830e68230
ik5 <idok@linesip.com>
2013-05-09 11:36:07 +0300

    Supporting the creation of the project

--- lib/scratch/actions.rb
+++ lib/scratch/actions.rb
@@ -6,16 +6,33 @@ module Scratch
     require 'fileutils'
 
     def self.copy_templates(dest)
+
     end
 
     class Exec
 
       def self.init(raw_args, args)
         if raw_args.length != 1
-          $stderr.puts CLI::error("No name was provided for the project")
+          $stderr.puts CLI::error('No name was provided for the project')
           exit(-3)
         end
-        
+
+        project_name = raw_args[0]
+        current_dir  = FileUtils.pwd
+
+        begin
+          FileUtils.mkdir(File.join(current_dir, project_name))
+          puts CLI::success("Created #{project_name}")
+        rescue Errno::EACCES
+          $stderr.puts CLI::error("Can not create #{project_name}, permission denied")
+          exit(-4)
+        rescue Errno::EEXIST
+          puts CLI::info("#{project_name} already exists, using it")
+        rescue => e
+          $stderr.puts CLI::error("Unknown error: #{e.message}")
+          exit(-5)
+        end
+
       end
 
       def self.test(raw_args, args)
